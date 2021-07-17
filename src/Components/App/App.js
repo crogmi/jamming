58886dc5a52f635db1c356d25bdcfbdbf01be4f7
crogmi <chrisogami16@gmail.com>
2021-07-17 12:20:57 -0700

    Updated tracklist.js to reflect maping over the array

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -10,7 +10,26 @@ class App extends React.Component {
     constructor(props) {
       super(props);
 
-      this.state = { searchResults: [{name: 'name1', album: 'album1', artist: 'artist1', id:1}, {name: 'name2', album: 'album2', artist: 'artist2', id:2}]}
+      this.state = { searchResults: [
+        {
+          name: 'name1', 
+          album: 'album1', 
+          artist: 'artist1', 
+          id:1
+        }, 
+        {
+          name: 'name2', 
+          album: 'album2', 
+          artist: 'artist2', 
+          id:2
+        }, 
+        {
+          name: 'name3', 
+          album: 'album3', 
+          artist: 'artist3', 
+          id:3
+        }
+      ]};
     }
 
     render () {
