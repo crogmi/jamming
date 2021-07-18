e81d4fd465ef5415ced993df6ec1a9e1babcc526
crogmi <chrisogami16@gmail.com>
2021-07-18 12:06:00 -0700

    Updating search functionality with Spotify API

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -22,6 +22,16 @@ const Spotify = {
             const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`;
             window.location = accessUrl;
         }
+    },
+
+    search(searchTerm) {
+        fetch(`https://api.spotify.com/v1/search?type=track&q=${searchTerm}`, {
+            headers: {Authorization: `Bearer ${accessToken}`}
+        }).then(response => {
+            if(response.ok) {
+                return response.json();
+            }
+        })
     }
 }
 
